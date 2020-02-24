2d5f74c4d3e79d7d6b1da7672c4b45151e563d5e
CharlesLiu02 <liu.charles02@gmail.com>
2020-02-24 09:18:05 -0800

    added counter to count number of ads on web page

--- content.js
+++ content.js
@@ -1,33 +1,41 @@
-
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(request.status);
-    foo = request.status;
-    check = request.initial;
-	return true;
-  });
+    function (request, sender, sendResponse) {
+        console.log(request.status);
+        foo = request.status;
+        check = request.initial;
+        return true;
+    });
 
 function highlighter(foo) {
-	
-	var highlight = document.getElementsByClassName('ads-visurl');
-	if(foo)
-	{
-		for(var i = 0; i < highlight.length; i++) {
-			highlight[i].style.background = '#ccff00';
-			highlight[i].getElementsByTagName('span')[0].style.background = '#ccff00';
-		}
-		
-	}
 
-	else {
-		for(var i = 0; i < highlight.length; i++) {
-			highlight[i].style.background = 'none';
-			highlight[i].getElementsByTagName('span')[0].style.background = 'none';
-		}
-		
-	}
+    var highlight = document.getElementsByClassName('ads-visurl');
+    if (foo) {
+        for (var i = 0; i < highlight.length; i++) {
+            highlight[i].style.background = '#ccff00';
+            highlight[i].getElementsByTagName('span')[0].style.background = '#ccff00';
+        }
+
+    } else {
+        for (var i = 0; i < highlight.length; i++) {
+            highlight[i].style.background = 'none';
+            highlight[i].getElementsByTagName('span')[0].style.background = 'none';
+        }
+
+    }
 }
+
+function countAds(foo) {;
+    if (foo) {
+        var numberAds = document.getElementsByClassName('ads-visurl').length;
+        chrome.runtime.sendMessage({greeting: "countAds", numberAds: numberAds}, function (response) {
+            console.log("message sent to popup");
+        });
+    }
+}
+
 var check;
 var foo;
-if(check)
-highlighter(foo);
+// if (check) {
+    highlighter(foo);
+    countAds(foo);
+// }
--- popup.js
+++ popup.js
@@ -1,41 +1,46 @@
+var numberOfAds;
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        numberOfAds = request.numberAds;
+    }
+)
+
 function updateLabel() {
-		 if(enabled == true) {
-			document.getElementById('toggle_button').value = "Disable"
-			foo = false;
-		}
-		else {
-			document.getElementById('toggle_button').value = "Enable"
-			foo = true;
-		}
-	}
+    if (enabled == true) {
+        document.getElementById('toggle_button').value = "Disable"
+        foo = false;
+    } else {
+        document.getElementById('toggle_button').value = "Enable"
+        foo = true;
+    }
+}
 
-	var enabled;
-	var foo;
-	var check = true;
-	document.getElementById('toggle_button').onclick = function () {
-		if(!enabled) {
-			enabled = true;
-		}
-		else {
-			enabled = false;
-		}
+var enabled;
+var foo;
+var check = true;
+document.getElementById('toggle_button').onclick = function () {
+    if (!enabled) {
+        enabled = true;
+    } else {
+        enabled = false;
+    }
 
-		chrome.tabs.query({ active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         // WAY 1
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello", status: foo, initital: check}), 
-        function() {
-        console.log('message sent')}
+        chrome.tabs.sendMessage(tabs[0].id, {status: foo, initital: check}),
+            function () {
+                console.log('message sent')
+            }
     });
-		chrome.tabs.executeScript({file: 'content.js'})
+    chrome.tabs.executeScript({file: 'content.js'})
 
-		updateLabel();
+    updateLabel();
+};
 
-	};
 
-
-	   
-   /*updateLabel()  
-   	 enabled is undefined so toggle_button.value = "Enable" */
+/*updateLabel()
+     enabled is undefined so toggle_button.value = "Enable" */
    
  
    
